132076560f18c2e5238c4abf92f98a97234af769
jin <jinyong.suh@navercorp.com>
2025-10-25 19:06:39 +0900

    웹 빌드 일단 추가

--- app/search/index.tsx
+++ app/search/index.tsx
@@ -3,12 +3,21 @@ import SearchWarehouseView from '@/components/search/SearchWarehouseView';
 import { useState } from 'react';
 import SearchWarehouse from '@/model/search/SearchWarehouse';
 import { useRouter } from 'expo-router';
+import AlertView from '@/components/alert/AlertView';
+import app from '@/model/app/App';
+import LogInView from '@/components/login/LogInView';
 
 const SearchPage = () => {
   const router = useRouter();
   const [searchWarehouse] = useState(() => SearchWarehouse.new(router));
 
-  return <SearchWarehouseView searchWarehouse={searchWarehouse} />;
+  return (
+    <>
+      <SearchWarehouseView searchWarehouse={searchWarehouse} />
+      <LogInView logInAlertManager={app.getLogInAlertManager()!} />
+      <AlertView alertManager={app.getAlertManager()!} />
+    </>
+  );
 };
 
 export default observer(SearchPage);
